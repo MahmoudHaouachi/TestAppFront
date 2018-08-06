653c8b9dc0a7278b40af628e7917aa55d60e204e
MahmoudHaouachi <39001429+MahmoudHaouachi@users.noreply.github.com>
2018-08-06 14:43:23 +0200

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -16,5 +16,4 @@ curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
 sudo yum install -y yarn
 # Install Angular CLI
 yarn global add @angular/cli@1.4.2
-Install node dependancies.
-yarn
+
