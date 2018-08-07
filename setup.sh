e92b430cad4876d74ba7392c0d310dc97cc2693c
MahmoudHaouachi <39001429+MahmoudHaouachi@users.noreply.github.com>
2018-08-07 12:05:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo su
 PUBLIC_IP=`wget http://169.254.169.254/latest/meta-data/public-ipv4 -O - -q ; echo`
 echo $PUBLIC_IP 
 cd /home/Angular/dist/test-ui/assets/data
-
+rm -rf appConfig.json
 echo '{ "serverURL" : "'"$PUBLIC_IP"'"}' > appConfig.json
 cd /home/Angular
 
