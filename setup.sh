fc3f5385e9838cc9aee1b39e0d3f19bd0e607503
MahmoudHaouachi <39001429+MahmoudHaouachi@users.noreply.github.com>
2018-08-14 11:44:15 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 sudo su 
 
+#configure nginx
+sudo rm -rf /etc/nginx/nginx.conf
+sudo mv /home/Angular/nginx.conf /etc/nginx/
+#restart nginx
+sudo service nginx restart
 
 PUBLIC_IP=`wget http://169.254.169.254/latest/meta-data/public-ipv4 -O - -q ; echo`
 echo $PUBLIC_IP 
