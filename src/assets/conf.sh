faeebabe32407b8dbe9ab8bff9c36cc1351fbd07
MahmoudHaouachi <39001429+MahmoudHaouachi@users.noreply.github.com>
2018-08-16 13:35:48 +0200

    Create conf.sh

---
+++ src/assets/conf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#configure nginx
+
+#restart nginx
+
+
+PUBLIC_IP=`wget http://169.254.169.254/latest/meta-data/public-ipv4 -O - -q ; echo`
+echo $PUBLIC_IP 
+cd /home/mahmoud/vstsagent/_work/r1/a/_FrontEnd/dist/test-ui/assets/data
+rm -rf appConfig.json
+echo '{ "serverURL" : "'"$PUBLIC_IP"'"}' > appConfig.json
