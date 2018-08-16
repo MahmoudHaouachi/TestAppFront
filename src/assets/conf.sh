4ee887c94cc03820ca60060b38b561ce726328f2
MahmoudHaouachi <39001429+MahmoudHaouachi@users.noreply.github.com>
2018-08-16 14:49:30 +0200

    Update conf.sh

--- src/assets/conf.sh
+++ src/assets/conf.sh
@@ -9,5 +9,5 @@
 PUBLIC_IP=`wget http://169.254.169.254/latest/meta-data/public-ipv4 -O - -q ; echo`
 echo $PUBLIC_IP 
 cd /home/mahmoud/vstsagent/_work/r1/a/_FrontEnd/dist/test-ui/assets/data
-rm -rf appConfig.json
+sudo rm -rf appConfig.json
 echo '{ "serverURL" : "'"$PUBLIC_IP"'"}' > appConfig.json
