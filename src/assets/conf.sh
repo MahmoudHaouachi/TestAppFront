d49b975c84cdbabf6276bfeff46291c534ceeb54
MahmoudHaouachi <39001429+MahmoudHaouachi@users.noreply.github.com>
2018-08-17 12:54:59 +0200

    Update conf.sh

--- src/assets/conf.sh
+++ src/assets/conf.sh
@@ -11,3 +11,5 @@ echo $PUBLIC_IP
 cd /home/mahmoud/vstsagent/_work/r1/a/_FrontEnd/dist/test-ui/assets/data
 sudo rm -rf appConfig.json
 echo '{ "serverURL" : "'"$PUBLIC_IP"'"}' > appConfig.json
+
+#restart nginx
