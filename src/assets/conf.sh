e6c79c2a5464bd099e9a644dd78996d858d251cc
MahmoudHaouachi <39001429+MahmoudHaouachi@users.noreply.github.com>
2018-08-17 13:03:21 +0200

    Update conf.sh

--- src/assets/conf.sh
+++ src/assets/conf.sh
@@ -8,7 +8,7 @@
 
 PUBLIC_IP=`curl ipinfo.io/ip`
 echo $PUBLIC_IP 
-cd /home/mahmoud/vstsagent/_work/r1/a/_FrontEnd/dist/test-ui/assets/data
+cd /home/mahmoud/vstsagent/_work/r1/a/_Frontend/dist/test-ui/assets/data
 sudo rm -rf appConfig.json
 echo '{ "serverURL" : "'"$PUBLIC_IP"'"}' > appConfig.json
 
