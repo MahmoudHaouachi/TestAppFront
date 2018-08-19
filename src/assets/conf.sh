e629165e1a0d71886d761767dcdf6a57215c3706
MahmoudHaouachi <39001429+MahmoudHaouachi@users.noreply.github.com>
2018-08-19 15:50:13 +0200

    Update conf.sh

--- src/assets/conf.sh
+++ src/assets/conf.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+#update
+sudo apt-get update
+
+#install nginx
+sudo apt-get install -y nginx
 
 #configure nginx
+sudo rm -rf /etc/nginx/nginx.conf
+sudo mv /home/mahmoud/vstsagent/_work/r1/a/_SoloFrontend CI/dist/test-ui/assets/nginx.conf /etc/nginx/
 
 
 
 
 PUBLIC_IP=`curl ipinfo.io/ip`
 echo $PUBLIC_IP 
-cd /home/mahmoud/vstsagent/_work/r1/a/_Frontend/dist/test-ui/assets/data
+cd /home/mahmoud/vstsagent/_work/r1/a/_SoloFrontend CI/dist/test-ui/assets/data
 sudo rm -rf appConfig.json
 echo '{ "serverURL" : "'"$PUBLIC_IP"'"}' > appConfig.json
 
 #restart nginx
+sudo service nginx restart
