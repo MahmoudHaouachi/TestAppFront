262ebf47fb99b468562dc5c61b29a5f9c3270faa
MahmoudHaouachi <39001429+MahmoudHaouachi@users.noreply.github.com>
2018-08-24 17:12:18 +0200

    Update DyIP.sh

--- src/assets/DyIP.sh
+++ src/assets/DyIP.sh
@@ -1,6 +1,6 @@
 
 PUBLIC_IP=`curl ipinfo.io/ip`
 echo $PUBLIC_IP 
-cd /usr/share/nginx/html/assets/data
-sudo rm -rf appConfig.json
+cd /app/TestAppFront/dist/test-ui/assets/data
+rm -rf appConfig.json
 echo '{ "serverURL" : "'"$PUBLIC_IP"'"}' > appConfig.json
